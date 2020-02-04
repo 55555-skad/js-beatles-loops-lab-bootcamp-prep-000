33c8aea718c8a4720041cf916a57de5077fe74d6
happy-clone-7156 <kaidornel@gmail.com>
2020-02-04 01:56:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,9 +34,10 @@ function iLoveTheBeatles(number){
   var array = []
   do {
     array.push("I love the Beatles!")
+       number++
   }
   while (number < 15 ){
-    number++
+ 
   }
   return array
   }
