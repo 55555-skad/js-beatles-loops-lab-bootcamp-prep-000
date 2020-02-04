5a541f4ef765d0b3e0dd8a43a6fde8fe69c7444c
happy-clone-7156 <kaidornel@gmail.com>
2020-02-04 02:03:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 function theBeatlesPlay(musicians, instruments){
   let newArray = []
   for(var i = 0; i < musicians.length; i++){
-    
+    newArray.push(musicians[i] + " plays " + instruments[i])
   }
   
   return newArray[]
