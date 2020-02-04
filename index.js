2c181fc8eca94a9ff7b3940e49ceffdd76ffd975
happy-clone-7156 <kaidornel@gmail.com>
2020-02-04 01:58:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,35 +10,3 @@
 //   return variable
 // }
 
-function theBeatlesPlay(musicians, instruments){
-  var newArray = []
-  
-  for(var i = 0; i < musicians.length; i++){
-    newArray.push(musicians[i] + " plays " + instruments[i])
-  }
-  
-  return newArray
-}
-
-function johnLennonFacts(facts){
-  var factArray = []
-  var count = 0
-  while(count < facts.length){
-    factArray.push(facts[count] + "!!!")
-    count++
-  }
-  return factArray
-}
-
-function iLoveTheBeatles(number){
-  var array = []
-  do {
-    array.push("I love the Beatles!")
-    number++
-  }
-  while (number < 15 ){
- 
-  }
-  return array
-  }
-  
